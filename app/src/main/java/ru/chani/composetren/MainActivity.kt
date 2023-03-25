84156a723d65132def56ad862cbae3305fa3f9ab
Dan <105542989+4aniDev@users.noreply.github.com>
2023-03-25 14:52:28 +0500

    lesson 2.5 - Card

--- app/src/main/java/ru/chani/composetren/MainActivity.kt
+++ app/src/main/java/ru/chani/composetren/MainActivity.kt
@@ -3,9 +3,12 @@ package ru.chani.composetren
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -27,6 +30,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
 
--- app/src/main/java/ru/chani/composetren/ui/theme/InstaProfileCard.kt
+++ app/src/main/java/ru/chani/composetren/ui/theme/InstaProfileCard.kt
@@ -1,7 +1,10 @@
 package ru.chani.composetren.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,26 +16,32 @@ import androidx.compose.ui.unit.dp
 @Preview(showBackground = true)
 @Composable
 fun InstaProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .width(500.dp)
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+
+    Card(
+        modifier = Modifier.padding(10.dp),
+        shape = RoundedCornerShape(topStart = 5.dp, topEnd = 5.dp),
+        border = BorderStroke(1.dp, Color.Black)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .padding(15.dp)
-                .size(80.dp)
-                .background(Color.Cyan)
+                .fillMaxWidth()
+                .width(500.dp)
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            Box(
+                modifier = Modifier
+                    .padding(15.dp)
+                    .size(80.dp)
+                    .background(Color.Cyan)
+            ) {
 
+            }
+            TwoBoxes(firstLine = "6,900", secondLine = "Post")
+            TwoBoxes(firstLine = "76", secondLine = "Followers")
+            TwoBoxes(firstLine = "756", secondLine = "Following")
         }
-        TwoBoxes(firstLine = "6,900", secondLine = "Post")
-        TwoBoxes(firstLine = "76", secondLine = "Followers")
-        TwoBoxes(firstLine = "756", secondLine = "Following")
-
     }
 }
 
