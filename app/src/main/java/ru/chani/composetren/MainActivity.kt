7a6b4a5377d22010e94c19ce9b47855939f8fbc9
Dan <105542989+4aniDev@users.noreply.github.com>
2023-03-30 18:03:34 +0500

    lesson 3.1 - Material Components, Slot APIs

--- app/src/main/java/ru/chani/composetren/MainActivity.kt
+++ app/src/main/java/ru/chani/composetren/MainActivity.kt
@@ -6,14 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.chani.composetren.ui.theme.ComposeTrenTheme
@@ -22,8 +23,53 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Test()
         }
     }
+}
+
+@Composable
+fun Test() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        ExampleThird()
+    }
+}
+
+@Composable
+fun ExampleOne() {
+    OutlinedButton(onClick = { /*TODO*/ }) {
+        Text(text = "Hello Ashgabat")
+    }
+}
+
+@Composable
+fun ExampleSecond() {
+    TextField(value = "Value",
+        onValueChange = {},
+        label = { Text(text = "Label") })
+}
+
+
+@Composable
+fun ExampleThird() {
+    AlertDialog(
+        onDismissRequest = { /*TODO*/ },
+        title = { Text(text = "Are you sure?") },
+        text = { Text(text = "Do you want to change your JOB?")},
+        confirmButton = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "Yeah!")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = "  No  ", textDecoration = TextDecoration.LineThrough)
+            }
+        },
+        shape = RoundedCornerShape(15)
+    )
 }
 
