a20cb9d4dc8a210d3e9429428f4f5191d8f7b2e8
Dan <105542989+4aniDev@users.noreply.github.com>
2023-03-31 11:24:56 +0500

    lesson 3.2 - Scaffold, TopAppBar, BottomNavigation (with BottomNavigationItem)

--- app/src/main/java/ru/chani/composetren/MainActivity.kt
+++ app/src/main/java/ru/chani/composetren/MainActivity.kt
@@ -9,10 +9,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,46 +40,85 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Test() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Top App Bar Title")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Menu, contentDescription = null)
+                    }
+                }
+            )
+        },
+        drawerContent = {
+            Text(
+                text = "Zero Text"
+            )
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ },
+                shape = RectangleShape
+            ) {
+                Text(text = "First Page")
+            }
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ },
+                shape = RectangleShape
+            ) {
+                Text(text = "Second Page")
+            }
+
+            IconButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ }
+            ) { Icon(Icons.Outlined.Clear, contentDescription = null)}
+
+
+        },
+        bottomBar = {
+            BottomNavigation {
+//                val items = listOf("First", "Second", "Third")
+//
+//                items.forEachIndexed { index, item ->
+//                    BottomNavigationItem(
+//                        icon = { Icon(Icons.Filled.Add, "") },
+//                        label = { Text(item) },
+//                        selected = index == 1,
+//                        onClick = {}
+//                    )
+//                }
+
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Favorite, null) },
+                    label = { Text("Favorite") },
+                    selected = true,
+                    onClick = {}
+                )
+
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Outlined.Add, null) },
+                    label = { Text("Plus") },
+                    selected = false,
+                    onClick = {}
+                )
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Outlined.Settings, null) },
+                    label = { Text("Settings") },
+                    selected = false,
+                    onClick = {}
+                )
+            }
+        }
     ) {
-        ExampleThird()
+        Text(
+            modifier = Modifier.padding(it),
+            text = "This is Scaffold content"
+        )
     }
 }
-
-@Composable
-fun ExampleOne() {
-    OutlinedButton(onClick = { /*TODO*/ }) {
-        Text(text = "Hello Ashgabat")
-    }
-}
-
-@Composable
-fun ExampleSecond() {
-    TextField(value = "Value",
-        onValueChange = {},
-        label = { Text(text = "Label") })
-}
-
-
-@Composable
-fun ExampleThird() {
-    AlertDialog(
-        onDismissRequest = { /*TODO*/ },
-        title = { Text(text = "Are you sure?") },
-        text = { Text(text = "Do you want to change your JOB?")},
-        confirmButton = {
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "Yeah!")
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = { /*TODO*/ }) {
-                Text(text = "  No  ", textDecoration = TextDecoration.LineThrough)
-            }
-        },
-        shape = RoundedCornerShape(15)
-    )
-}
-
