53c9b60d1d5efa6d35ce3cae4a25c8129fd3e79d
Dan <105542989+4aniDev@users.noreply.github.com>
2023-04-03 13:40:13 +0500

    lesson 3.4 - rememberCoroutineScore(), Snackbar, FAB

--- app/src/main/java/ru/chani/composetren/ui/VkNewMainScreen.kt
+++ app/src/main/java/ru/chani/composetren/ui/VkNewMainScreen.kt
@@ -1,20 +1,58 @@
 package ru.chani.composetren.ui
 
 import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 private const val DEFAULT_INDEX_OF_SELECTED_ITEM = 0
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val action = snackbarHostState.showSnackbar(
+                                message = "This is Snackbar",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+
+                            if (action == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }
+                ) {
+                    Icon(Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+
+        },
         bottomBar = {
             BottomNavigation {
+                Log.d("TAG_TEST", "recomposition of BottomNavigation")
 
                 val selectedItemIndex = remember {
                     mutableStateOf(DEFAULT_INDEX_OF_SELECTED_ITEM)
@@ -43,6 +81,7 @@ fun MainScreen() {
                 }
             }
         }
+
     ) {
 
     }
